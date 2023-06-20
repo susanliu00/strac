6d210ce99ad24776fc267302f95baf5c4edc3f8c
Susan Liu <susanyliu00@gmail.com>
2023-06-20 12:49:19 -0400

    CSS

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-// import { w3cwebsocket as WebSocket } from "websocket";
 import React, { useEffect, useState } from "react";
 
 let url = "http://localhost:8000";
+
 const File = ({ backendData }) => {
   return (
     <div className="file">
-      {/* <h2>Id: {backendData[0]}</h2> */}
       <div>
-        <h3>Name: {backendData[1][0]}</h3>
-        <h3>Users</h3>
-        {backendData[1][1].map((u) => {
-          return <div>{u.displayName}</div>;
-        })}
+        <h3 className="name-header">{backendData[1][0]}</h3>
+        <div className="users">
+          <h3 className="users-header">Users</h3>
+          <ul>
+            {backendData[1][1].map((u) => {
+              return <div>{u.displayName}</div>;
+            })}
+          </ul>
+        </div>
+
         <div className="download">
           <button
             onClick={() => {
@@ -24,7 +28,6 @@ const File = ({ backendData }) => {
                   name: backendData[1][0],
                 })
                 .then((res) => console.log(res));
-              console.log("clickede");
             }}
           >
             Download
@@ -37,35 +40,32 @@ const File = ({ backendData }) => {
 
 function App() {
   const [files, setFiles] = useState({});
-  const events = new EventSource(`${url}/events`);
 
   useEffect(() => {
     axios.get(`${url}/files`).then((data) => {
       setFiles(data.data);
       console.log("FILES", data.data);
     });
-  });
+  }, [files]);
 
   useEffect(() => {
+    const events = new EventSource(`${url}/events`);
     events.onmessage = (event) => {
       const parsedData = JSON.parse(event.data);
-      console.log("PARSED", parsedData);
       if (parsedData.length === 1) {
         const remove = { ...files };
         delete remove[parsedData[0]];
-        console.log("REMOVED", remove);
         setFiles(remove);
       } else {
         const key = parsedData[0];
         setFiles({ ...files, [key]: [parsedData[1], parsedData[2]] });
       }
     };
-  });
+  }, [files]);
 
   return (
     <div className="App">
       {Object.entries(files).map(([id, perm]) => {
-        console.log(id, perm);
         return <File backendData={[id, perm]} />;
       })}
     </div>
